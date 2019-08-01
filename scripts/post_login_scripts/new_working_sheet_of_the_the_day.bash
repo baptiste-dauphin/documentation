ff4fcf0fd15905c958a50450261570e0bc3b9bd1
Baptiste Dauphin <b.dauphin@qwant.com>
2019-08-01 14:55:04 +0200

    improve new working sheet of the day

--- scripts/post_login_scripts/new_working_sheet_of_the_the_day.bash
+++ scripts/post_login_scripts/new_working_sheet_of_the_the_day.bash
@@ -3,20 +3,28 @@
 DIR=$1
 FILE_OF_THE_DAY_LOCATION="$DIR/$(date +%Y_%m_%d.bash)"
 
-if [[ -e $FILE_OF_THE_DAY_LOCATION ]]
 
+if [ "$#" -ne 1 ]
 then
-	echo "file of the day $FILE_OF_THE_DAY_LOCATION already exists"
-	echo "you can write on it !!!"
-	echo "subl $FILE_OF_THE_DAY_LOCATION"
-	exit 1
-
-else 
-	echo "Creation of $FILE_OF_THE_DAY_LOCATION"
-	echo "you can :"
-	echo "subl $FILE_OF_THE_DAY_LOCATION"
-	exit 0
+    echo "Illegal number of parameters"
+    echo "you have to specify a directory where store working sheet !!!"
+    echo "example : new_working_sheet_of_the_the_day /home/baptiste/Documents"
+    exit 1
+else
+	if [[ -e $FILE_OF_THE_DAY_LOCATION ]]
+	then
+		echo "file of the day $FILE_OF_THE_DAY_LOCATION already exists"
+		echo "you can write on it !!!"
+		echo "subl $FILE_OF_THE_DAY_LOCATION"
+		exit 1
+	else 
+		echo "Creation of $FILE_OF_THE_DAY_LOCATION"
+		echo "you can :"
+		echo "subl $FILE_OF_THE_DAY_LOCATION"
+		exit 0
+	fi
 fi
+
 
 
 
