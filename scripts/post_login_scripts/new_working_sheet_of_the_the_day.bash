e84dec029dd4e3ad37531fc79bdf7b3254a1f74d
Baptiste Dauphin <b.dauphin@qwant.com>
2019-08-05 12:12:16 +0200

    improve new working sheet of the day

--- scripts/post_login_scripts/new_working_sheet_of_the_the_day.bash
+++ scripts/post_login_scripts/new_working_sheet_of_the_the_day.bash
@@ -14,13 +14,14 @@ else
 	if [[ -e $FILE_OF_THE_DAY_LOCATION ]]
 	then
 		echo "file of the day $FILE_OF_THE_DAY_LOCATION already exists"
-		echo "you can write on it !!!"
-		echo "subl $FILE_OF_THE_DAY_LOCATION"
-		exit 1
+		echo "Opening document..."
+		subl $FILE_OF_THE_DAY_LOCATION
+		exit 2
 	else 
 		echo "Creation of $FILE_OF_THE_DAY_LOCATION"
-		echo "you can :"
-		echo "subl $FILE_OF_THE_DAY_LOCATION"
+		touch $FILE_OF_THE_DAY_LOCATION
+		echo "Opening document..."
+		subl $FILE_OF_THE_DAY_LOCATION
 		exit 0
 	fi
 fi
