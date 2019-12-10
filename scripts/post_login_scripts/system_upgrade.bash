5e9cf883b928c660edad8cea83f837e5d32eb4c6
Baptiste Dauphin <b.dauphin@qwant.com>
2019-12-10 09:21:48 +0100

    add list_upgradable to post_login scripts

--- scripts/post_login_scripts/system_upgrade.bash
+++ scripts/post_login_scripts/system_upgrade.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+list_upgradable(){
+    apt list --upgradable;
+};
+
 patching(){
     # sudo specified because this was from my bootstrap script for an
     # ec2 instance remove the sudos as previously stated if you are 
@@ -9,4 +13,5 @@ patching(){
     sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y;
 };
 
+list_upgradable;
 patching;
