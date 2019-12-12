afad7a8354e8d61c53fd6c4e4639df4e1e020bd1
Baptiste Dauphin <b.dauphin@qwant.com>
2019-12-12 11:46:17 +0100

    improve system_upgrade.bash

--- scripts/post_login_scripts/system_upgrade.bash
+++ scripts/post_login_scripts/system_upgrade.bash
@@ -1,17 +1,43 @@
 #!/usr/bin/env bash
 set -e
 
-list_upgradable(){
-    apt list --upgradable;
+function display () {
+	echo "---------------------------------------------"
+	echo "[+]  $1"
+	echo "---------------------------------------------"
+	echo ""
+} 
+
+function display_separator () {
+	echo ""
+	echo "---------------------------------------------"
+	echo ""
+} 
+
+function print_apt_repositories(){
+	display 'Here are your configured APT repositories'
+	../get_remote_apt_reposotires_url.bash;
+	display_separator
 };
 
-patching(){
+function list_upgradable(){
+	display 'Here are a list of upgradable packages'
+    apt list --upgradable;
+    display_separator
+};
+
+function patching(){
     # sudo specified because this was from my bootstrap script for an
     # ec2 instance remove the sudos as previously stated if you are 
     # going to exec it from your current shell
+    display 'Running upgrade of all your packages (including linux version)'
     sudo apt-get update -y;
     sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y;
+    display_separator
 };
 
+
+# main
+print_apt_repositories;
 list_upgradable;
 patching;
