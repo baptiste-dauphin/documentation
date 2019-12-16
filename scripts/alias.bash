dcc394e6add0a5ee0cbf85ab734b1645de8c94b6
Baptiste Dauphin <b.dauphin@qwant.com>
2019-12-16 18:36:18 +0100

    add 'get git branch name' command

--- scripts/alias.bash
+++ scripts/alias.bash
@@ -3,6 +3,7 @@ alias la='ls -A'
 alias l='ls -CF'
 alias hashtag="echo \"grep -iv '^\s*[#\;]\|^\s*$'\""
 alias post_login="bash ~/Git/documentation/scripts/post_login_scripts/startup.sh"
+alias ggbn="git branch | grep \* | cut -d ' ' -f2"
 
 if [ $commands[ccze] ]; then
   alias color="ccze -A"
