4bda73c7816d6916ac8f7a094474dd5e9f75081d
Baptiste Dauphin <b.dauphin@qwant.com>
2020-01-15 09:39:38 +0100

    remove keyboard-fr, pulseaudio start from startup cmd

--- scripts/post_login_scripts/startup.sh
+++ scripts/post_login_scripts/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DIR="$(dirname "$0")"
 
-$DIR/keyboard-fr.sh
+# $DIR/keyboard-fr.sh
 # $DIR/monitors-placement.sh
-$DIR/pulseaudio-start.sh
+# $DIR/pulseaudio-start.sh
+# $DIR/run_keybase.bash
 $DIR/ssh_agent.sh
 $DIR/system_upgrade.bash
-# $DIR/run_keybase.bash
